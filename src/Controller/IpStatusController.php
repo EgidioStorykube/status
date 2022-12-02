96b13d863014b4e04f5edd79e48e132149d5a31c
Egidio <egidio.speranza@storykube.com>
2022-12-02 17:08:02 +0100

    Color and box for Monitoring pages

--- src/Controller/IpStatusController.php
+++ src/Controller/IpStatusController.php
@@ -10,7 +10,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Validator\Constraints\Length;
 
 use function PHPSTORM_META\type;
 
